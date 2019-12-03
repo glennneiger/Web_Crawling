228706371ca241d7a9a7d993eb70dfdb1ec1dd5a
SsenSeungE <57127381+kkm8257@users.noreply.github.com>
2019-12-03 15:55:59 +0900

    12_03

--- Crawling_Prac5.py
+++ Crawling_Prac5.py
@@ -7,26 +7,21 @@ url='https://news.naver.com/main/list.nhn?mode=LS2D&sid2=230&sid1=105&mid=shm&da
 html=urllib.request.urlopen(url).read() 
 
 bs_obj = BeautifulSoup(html,'html.parser') 
-<<<<<<< HEAD
-step1=bs_obj.select('div.list_body.newsflash_body > ul > li > dl')
 
-a=[]
+step1=bs_obj.select('div.list_body.newsflash_body > ul > li > dl ')
+
+count=0
+
+
+
+
 
 for step in step1:
-    a = step.find_all('a') # 리스트 a 에다가 step1 하위에 속한 태그 a 값들을 넣고
-    
-    
-
-
+    a = step.find_all('a')
 
     for b in a: #for문으로 차례로 출력
         print(b.text.strip())
         
-      
-
-    if(len(a)>0):        
-        print(a[0].get('href'))
-
-   
-
+    if (len(a) > 0):
+        print(a[0].get("href"))
 
